03e7fcb238b5319e570f16a0d22feeec7f72963b
Madis-Julius <estonira@gmail.com>
2022-04-08 17:07:30 +0300

    Made alot of new stuff like hooolee shiiiieeed

---
+++ MadislolincWeb/Data/News.cs
@@ -0,0 +1,10 @@
+﻿namespace MadislolincWeb.Data
+{
+    public class News
+    {
+        public String Title { get; set; }
+        private String _imageName = "Default.jpg";
+        public String ImageName { get { return _imageName; } set { _imageName = value; } }
+        public String Story { get; set; }
+    }
+}
---
+++ MadislolincWeb/Data/NewsDataFetcher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace MadislolincWeb.Data
+{
+    public class NewsDataFetcher
+    {
+        public Task<News[]> GetNewsAsync()
+        {
+
+            String datatext = File.ReadAllText("wwwroot/data/News.json");
+            News[] news = JsonSerializer.Deserialize<News[]>(datatext);
+
+            return Task.FromResult(Enumerable.Range(0, news.Length).Select(Index => new News
+            {
+                Title = news[Index].Title,
+                ImageName = news[Index].ImageName,
+                Story = news[Index].Story
+            }
+            ).ToArray());
+        }
+
+    }
+}
